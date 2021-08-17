5f29748011227e2bcaf70d6163cb1f26a9cc9d8d
NerveCell <deploint@gmail.com>
2021-08-17 19:33:23 +0300

    Add controller script

---
+++ Mitra_2D_Platformer/Assets/Scripts/Player/PlayerCntrl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCntrl : MonoBehaviour
+{
+    // Movement mov;
+    void Start()
+    {
+        // mov = GetCompoent<Movement>();
+    }
+    void Update()
+    {
+        // if (Input.GetKey(KeyCode.D)) { mov.Run(); }
+        // if (Input.GetKey(KeyCode.W)) { mov.Jump(); }
+    }
+}
