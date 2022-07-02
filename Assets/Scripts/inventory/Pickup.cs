6b550034490e7f40ad3d9a549ac9df966ecc26e7
VseleiyM <goodvaselei2001@mail.ru>
2022-07-02 23:10:09 +0300

    main

---
+++ Assets/Scripts/inventory/Pickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private InventoryM inventory;
+    public GameObject slotButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Playre").GetComponent<InventoryM>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Playre"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if(inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                }
+            }
+        }
+    }
+}
