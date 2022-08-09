0762257745725c2a3484e097e3f51c477a988dc9
VseleiyM <goodvaselei2001@mail.ru>
2022-08-09 20:22:13 +0300

    TestBranch_InvantoreLit_PlatformFul

---
+++ Assets/Scripts/inventory/InventoryM.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryM : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] slots;
+    public GameObject inventotry;
+    private bool inventotryOn;
+
+    private void Start()
+    {
+        inventotryOn = false;
+    }
+
+    public void Chest()
+    {
+        if (inventotryOn == false)
+        {
+            inventotryOn = true;
+            inventotry.SetActive(true);
+        }
+        else if(inventotryOn == true)
+        {
+            inventotryOn = false;
+            inventotry.SetActive(false);
+        }
+
+    }
+}
